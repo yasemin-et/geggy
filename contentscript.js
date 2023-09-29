dc8f605ac2f418b0edd4ada54a61905d3af3bf42
yasemin-et <yasemin.elif.turkoglu@outlook.com>
2023-09-29 15:30:11 -0400

    Game now starts without crashing

--- contentscript.js
+++ contentscript.js
@@ -7,69 +7,32 @@ function readMessage(message) {
   }
 }
 
+
+window.animatorDone = false;
+window.cameraDone = false;
+window.platformDone = false;
+window.playerDone = false;
+window.scoreDone = false;
+window.graphicsDone = false;
+window.physicsDone = false;
+
 function startGame() {
-  // Add new scripts here to be injected on game start
+    // Add new scripts here to be injected on game start
+    const scripts = ["src/js/animator.mjs", "src/js/platformGeneration.js", "src/js/camera.js", "src/js/score.js", "src/js/graphics.js", "src/js/physics.js", "src/js/player.js"];
+    scripts.forEach(loadFile);  
 
-  (async () => {
-    const src = chrome.runtime.getURL("src/js/animator.mjs");
-    const contentMain = await import(src);
-    console.log(contentMain);
-    contentMain.default();
-  })();
-
-  (async () => {
-    const src = chrome.runtime.getURL("src/js/platformGeneration.js");
-    const contentMain = await import(src);
-    console.log(contentMain);
-    contentMain.default();
-  })();
-
-  (async () => {
-    const src = chrome.runtime.getURL("src/js/camera.js");
-    const contentMain = await import(src);
-    console.log(contentMain);
-    contentMain.default();
-  })();
-
-  (async () => {
-    const src = chrome.runtime.getURL("src/js/score.js");
-    const contentMain = await import(src);
-    console.log(contentMain);
-    contentMain.default();
-  })();
-
-  (async () => {
-    const src = chrome.runtime.getURL("src/js/graphics.js");
-    const contentMain = await import(src);
-    console.log(contentMain);
-    contentMain.default();
-  })();
-
-  (async () => {
-    const src = chrome.runtime.getURL("src/js/physics.js");
-    const contentMain = await import(src);
-    console.log(contentMain);
-    contentMain.default();
-  })();
-
-  (async () => {
-    const src = chrome.runtime.getURL("src/js/player.js");
-    const contentMain = await import(src);
-    console.log(contentMain);
-    contentMain.default();
-  })();
-  
-  (async () => {
-    const src = chrome.runtime.getURL("src/js/game_runner.js");
-    const contentMain = await import(src);
-    console.log(contentMain);
-    contentMain.default();
-  })();
-
-  console.log("Starting...");
-
+    // wait for all scripts to be loaded
+    // yes this is a bad way to do it but i dont have time to learn webpack
+    delay(100).then(() => loadFile("src/js/game_runner.js"));
 }
 
-async function sendScore() {
-  
+async function loadFile(filepath) {
+    const src = chrome.runtime.getURL(filepath);
+    const contentMain = await import(src);
+    console.log(contentMain);
+    contentMain.default();
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
 }
